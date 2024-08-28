438ddfa714ff38158f2c1015e3189c0166f11645
Pritinalkar <rutujanalkar07@gmail.com>
2024-08-28 09:43:34 +0530

    largest number program added

---
+++ largest.js
@@ -0,0 +1,11 @@
+const numbers =[30,70,1500,240,230,240]
+
+let largetsNumber = numbers[0]
+
+ for(number of numbers ){
+     if(largetsNumber<number){
+         largetsNumber = number
+     }
+ }
+
+ console.log(`largest number is: ${largetsNumber}`)
