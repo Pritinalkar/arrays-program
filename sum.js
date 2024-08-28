5bce35e4abdd8da77de779d11cd7044fe41f30f1
Pritinalkar <rutujanalkar07@gmail.com>
2024-08-28 09:46:28 +0530

    sum of odd number program added

---
+++ sum.js
@@ -0,0 +1,11 @@
+const numbers = [9,2,6,4,5,,12,17,16,39]
+
+let sum =0 
+  
+ for(number of numbers){
+     if(number%2==1){
+         sum+=number
+     }
+ }
+
+ console.log(`sum of odd numbers is ${sum}`)
